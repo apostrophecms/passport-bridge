9c442bc820bd2d046fa3dcb68556d0fabdbbf835
Thomas Boutell <tom@apostrophecms.com>
2022-01-04 10:42:44 -0500

    * Support npm modules that export their strategy as a `.Strategy` property, as well as those that export it directly * Document the implications of not using the `create` option, although it still would be too risky to turn on in the standard example * Run quiet

--- index.js
+++ index.js
@@ -21,10 +21,11 @@ module.exports = {
         }
 
         self.options.strategies.forEach(spec => {
-          // console.log(spec.module);
-          // console.log(spec.Strategy);
-          const Strategy = spec.module ? self.apos.root.require(spec.module).Strategy : spec.Strategy;
-          console.log(Strategy);
+          // Works with npm modules that export the strategy directly, npm modules
+          // that export a Strategy property, and directly passing in a strategy property
+          // in the spec
+          const strategyModule = spec.module && self.apos.root.require(spec.module);
+          const Strategy = strategyModule ? (strategyModule.Strategy || strategyModule) : spec.Strategy;
           if (!Strategy) {
             throw new Error('@apostrophecms/passport-bridge: each strategy must have a "module" setting\n' +
               'giving the name of an npm module installed in your project that\n' +
@@ -117,7 +118,8 @@ module.exports = {
         self.apos.app.get(self.getCallbackUrl(spec, false),
           // middleware
           self.apos.login.passport.authenticate(
-            spec.name, {
+            spec.name,
+            {
               failureRedirect: self.getFailureUrl(spec)
             }
           ),
@@ -134,7 +136,6 @@ module.exports = {
 
       addFailureRoute(spec) {
         self.apos.app.get(self.getFailureUrl(spec), function (req, res) {
-          console.log(spec);
           // Gets i18n'd in the template
           return self.sendPage(req, 'error', {
             spec: spec,
